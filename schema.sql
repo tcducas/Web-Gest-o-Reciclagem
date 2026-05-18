80fd3c59cda6aef9382e9b19e8c6ada4a42ef0c0
tcducas <thacanducas@icloud.com>
2026-05-18 22:00:25 +0000

    web_reciclagem_apresentacao

--- schema.sql
+++ schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
 -- 2. Tabela de Materiais Recicláveis
 CREATE TABLE IF NOT EXISTS materiais (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL, -- Ex: Plástico, Papel, Eletrônicos
+    nome TEXT NOT NULL,
     descricao TEXT,
     categoria TEXT
 );
@@ -20,11 +20,18 @@ CREATE TABLE IF NOT EXISTS pontos_coleta (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome_local TEXT NOT NULL,   
     endereco TEXT NOT NULL,
-    -- Campos Cruciais:
-    horario_funcionamento TEXT NOT NULL, -- Tag importante para o usuário
-    telefone_contato TEXT,
-    tipo_residuo_aceito TEXT,              
+    horario_funcionamento TEXT NOT NULL, 
+    telefone_contato TEXT,             
     usuario_id INTEGER,                    
     data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (usuario_id) REFERENCES usuarios (id)
+);
+
+-- 4. Tabela Associativa (Relacionamento N:N entre Pontos e Materiais)
+CREATE TABLE IF NOT EXISTS pontos_coleta_materiais (
+    ponto_coleta_id INTEGER,
+    material_id INTEGER,
+    PRIMARY KEY (ponto_coleta_id, material_id),
+    FOREIGN KEY (ponto_coleta_id) REFERENCES pontos_coleta(id),
+    FOREIGN KEY (material_id) REFERENCES materiais(id)
 );
